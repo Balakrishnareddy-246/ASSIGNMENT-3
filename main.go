0da1ba3b423e54079dc0c4556f32d36d608f48c0
Kanakanti Balakrishnareddy <55091139+balakrishnareddy246@users.noreply.github.com>
2020-11-13 14:01:22 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"github.com/rahmanfadhil/gin-bookstore/controllers"
-	"github.com/rahmanfadhil/gin-bookstore/models"
-
-	"github.com/gin-gonic/gin"
+	"github.com/mingrammer/go-todo-rest-api-example/app"
+	"github.com/mingrammer/go-todo-rest-api-example/config"
 )
 
 func main() {
-	r := gin.Default()
+	config := config.GetConfig()
 
-	// Connect to database
-	models.ConnectDatabase()
-
-	// Routes
-	r.GET("/books", controllers.FindBooks)
-	r.GET("/books/:id", controllers.FindBook)
-	r.POST("/books", controllers.CreateBook)
-	r.PATCH("/books/:id", controllers.UpdateBook)
-	r.DELETE("/books/:id", controllers.DeleteBook)
-
-	// Run the server
-	
+	app := &app.App{}
+	app.Initialize(config)
+	app.Run(":3000")
+}
